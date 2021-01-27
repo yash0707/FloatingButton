11273f761d86f33df5ed52ab68dd662cd816b2b8
Yash Varshney <yashvarshney@urbanclap.com>
2021-01-27 22:43:27 +0530

    Initial Commit

---
+++ FloatingButton/FloatingButton.h
@@ -0,0 +1,18 @@
+//
+//  FloatingButton.h
+//  FloatingButton
+//
+//  Created by Yash  Varshney on 27/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FloatingButton.
+FOUNDATION_EXPORT double FloatingButtonVersionNumber;
+
+//! Project version string for FloatingButton.
+FOUNDATION_EXPORT const unsigned char FloatingButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatingButton/PublicHeader.h>
+
+
